ba3f24b9e70e3486d2eb2bfe9c7ffac9a9345db7
HarlemChan <3166144559@qq.com>
2023-02-19 22:20:44 +0800

    update

--- Doctor Workstation/Form1.cs
+++ Doctor Workstation/Form1.cs
@@ -19,7 +19,7 @@ namespace Doctor_Workstation
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("123");
+            MessageBox.Show("123"); MessageBox.Show("123");
         }
     }
 }
